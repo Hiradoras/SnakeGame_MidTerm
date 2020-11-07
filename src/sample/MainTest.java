c08c79b743eabe128470806872e93b9532d3f186
HolyLoki <forwoermind@gmail.com>
2020-11-07 14:54:47 +0600

    Junit testes addded

---
+++ src/sample/MainTest.java
@@ -0,0 +1,22 @@
+package sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void start() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void tick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void newFood() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
